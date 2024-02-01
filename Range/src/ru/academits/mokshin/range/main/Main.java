4afdfca293f6157e280f2977b37827c84703f909
mokshinpv <mokshinpv@gmail.com>
2024-02-01 19:48:41 +0700

    Update Main.java

--- Range/src/ru/academits/mokshin/range/main/Main.java
+++ Range/src/ru/academits/mokshin/range/main/Main.java
@@ -4,6 +4,7 @@ import ru.academits.mokshin.range.Range;
 
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range();
+        Range range1 = new Range();
+        Range range2 = new Range();
     }
 }
